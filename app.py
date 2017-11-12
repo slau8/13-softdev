2f0c133af9ea90e91dc09e4bce1f011f2066ab99
Shannon Lau <shannonlau00@gmail.com>
2017-11-11 21:46:05 -0500

    woohooooo

--- app.py
+++ app.py
@@ -5,28 +5,45 @@ HW13 -- A RESTful Journey Skyward
 2017-11-10
 '''
 
-from flask import Flask, render_template, request, Markup
-import urllib2, json
-import random
+from flask import Flask, render_template, request, redirect, flash, Markup
+import urllib2, json, random, os
 
 app = Flask(__name__)
+app.secret_key = os.urandom(128)
 
+# root route // access and render NASA information
 @app.route("/")
+def nasa():
+    url = "https://api.nasa.gov/planetary/apod?api_key=fIL3dEQ6aMLbaWIaeDpwT43wQjryP1zNUJCio2SZ"
+    # access data as a {}
+    d = retrieve_data(url)
+    return render_template('nasa.html', img = d["url"], caption = d["explanation"])
+
+# render homepage
+@app.route("/home")
 def home():
     return render_template("home.html")
 
+# find and display random job under specified keyword
 @app.route("/job", methods=['GET','POST'])
 def job():
-    source = "https://jobs.github.com/positions.json?description="
-    lang = request.args['lang']
-    source += lang
-    data = urllib2.urlopen(source)
-    # access job listings in a []
-    d = json.loads(data.read())
+    # check if keyword was requested
+    if not request.args.get('keyword'):
+        return redirect('home')
+    # retrieve and format source
+    keyword = request.args['keyword']
+    qkeyword = keyword.replace(" ","+")
+    url = "https://jobs.github.com/positions.json?description=" + qkeyword
+    # access job listings as a [] of {}
+    l = retrieve_data(url)
+    # check if there are no results
+    if l == []:
+        flash("Nothing found for " + keyword)
+        return redirect('home')
     # access random job in a {}
-    job = random.choice(d)
+    job = random.choice(l)
     return render_template("job.html",
-                            lang = lang,
+                            keyword = keyword,
                             title = job["title"],
                             type = job["type"],
                             location = job["location"],
@@ -34,27 +51,15 @@ def job():
                             description = Markup(job["description"]),
                             apply = Markup(job["how_to_apply"]))
 
-
-'''
-@app.route("/")
-def hello_world():
-    source = "https://api.nasa.gov/planetary/apod?api_key=fIL3dEQ6aMLbaWIaeDpwT43wQjryP1zNUJCio2SZ"
-    data = urllib2.urlopen(source)
-    print "\n\nURL:"
+# opens URL to be read and returns JSON data as a dict/list
+def retrieve_data(url):
+    data = urllib2.urlopen(url)
+    print "\n\nURL: "
     print data.geturl()
-    print "\n\nHEADER INFO:"
+    print "\n\nINFO: "
     print data.info()
-    print "\n\nSOURCE CODE:"
-    # access API data
-    data_string = data.read()
-    print data_string
-    # convert data into a dict
-    d = json.loads(data_string)
-    img = d["url"]
-    caption = d["explanation"]
-    return render_template('nasa.html', img = img, caption = caption)
-'''
-
+    d = json.loads(data.read())
+    return d
 
 if __name__ == "__main__":
     app.debug = True
